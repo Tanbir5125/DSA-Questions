2946c94b9f25f0afba7cd4eccb4b17df9ddc0825
TANBIR AHAMED <tanbirahamedg@gmail.com>
2024-07-10 02:07:21 +0530

    find divisors of numbers

---
+++ divisors.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+void findDivisors(int n){
+    vector<int>ls;
+    for(int i=1;i<=sqrt(n);i++){
+        
+    }
+}
+
+
+int main() {
+    int n;
+    cout<<"Enter a Number: ";
+    cin>>n;
+    findDivisors(n);
+    return 0;
+}
