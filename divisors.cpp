4f2fbda3fe7a9d7f97b62d78c5cf2cb5e74d45dc
TANBIR AHAMED <tanbirahamedg@gmail.com>
2024-07-10 02:18:39 +0530

    completed program to find divisors of a number

--- divisors.cpp
+++ divisors.cpp
@@ -3,8 +3,15 @@
 void findDivisors(int n){
     vector<int>ls;
     for(int i=1;i<=sqrt(n);i++){
-        
+        if(n % i == 0)
+            ls.push_back(i);
+        if(n/i != i)
+            ls.push_back(n/i);
     }
+    sort(ls.begin(),ls.end());
+    for(auto it : ls)
+        cout<<it<<" ";
+    cout<<endl;
 }
 
 
