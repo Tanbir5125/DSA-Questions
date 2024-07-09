6d120bb9741d7309d4104abca1636c9534e918bd
TANBIR AHAMED <tanbirahamedg@gmail.com>
2024-07-10 02:45:49 +0530

    added comments

--- divisors.cpp
+++ divisors.cpp
@@ -1,11 +1,11 @@
 #include <bits/stdc++.h>
 
 void findDivisors(int n){
-    vector<int>ls;
-    for(int i=1;i*i<=n;i++){
-        if(n % i == 0)
+    vector<int>ls;  //used vector to store the divisors
+    for(int i=1;i*i<=n;i++){  // to reduce time complexity used i*i instead of sqrt(n)
+        if(n % i == 0)  //to check if the i can divide the number completely
             ls.push_back(i);
-        if(n/i != i)
+        if(n/i != i)  // if i is a divisor then n / i is also a divisor
             ls.push_back(n/i);
     }
     sort(ls.begin(),ls.end());
