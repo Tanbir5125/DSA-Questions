c06435c236b42c52ca66e02628b029d7e74db269
TANBIR AHAMED <tanbirahamedg@gmail.com>
2024-08-10 20:53:44 +0530

    formatted selection_sort.cpp

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -1,21 +1,24 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-void selectionSort(int arr[], int n){
-    for(int i = 0;i < n-1; i++){
+void selectionSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
         int min = i;
-        for(int j = i+1;j < n; j++)
-            if(arr[j] < arr[min])
-                min  = j;
-        swap(arr[i],arr[min]);
+        for (int j = i + 1; j < n; j++)
+            if (arr[j] < arr[min])
+                min = j;
+        swap(arr[i], arr[min]);
     }
 }
 
-int main(){
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt","r",stdin);
-    freopen("output.txt","w",stdout);
-    #endif
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
     // number of elements in the array
     int n;
     // cout<<"Enter the number of elements in the array: ";
@@ -24,25 +27,26 @@ int main(){
     int arr[n];
 
     // take inputs for the arr
-    for(int i=0;i<n;i++){
+    for (int i = 0; i < n; i++)
+    {
         cin >> arr[i];
     }
 
     // Before Sorting
-    cout<<"Before Sorting: "<<endl;
-    for(auto it : arr)
-        cout << it<<" ";
-    cout<<endl;
+    cout << "Before Sorting: " << endl;
+    for (auto it : arr)
+        cout << it << " ";
+    cout << endl;
 
     // function call
 
     selectionSort(arr, n);
 
     // After Sorting
-    cout<<"After Sorting: "<<endl;
-    for(auto it : arr)
-        cout << it<<" ";
-    cout<<endl;
+    cout << "After Sorting: " << endl;
+    for (auto it : arr)
+        cout << it << " ";
+    cout << endl;
 
     return 0;
 }
